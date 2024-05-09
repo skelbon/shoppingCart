1d2c2b7bd7948175041ec5ecbe113a99ab129447
skelbon <spuriousmeaning@gmail.com>
2024-05-09 16:50:25 +0100

    Getting started with the checkout class constructor

--- lib/Checkout.pm
+++ lib/Checkout.pm
@@ -3,4 +3,19 @@ package Checkout;
 use strict;
 use warnings;
 
+#constructor
+sub new {
+
+    my($class, $products) = @_;
+
+    my $self = {
+        products => { map { $_->get_item_code => $_ @$products}},
+        subtotal => 0,
+        basket = {}
+    }
+
+    bless $self, $class;
+    return $self;
+}
+
 1;
--- lib/Product.pm
+++ lib/Product.pm
@@ -9,7 +9,7 @@ sub new {
 
     # input validation
 
-    _validate_discount_scheme_input($discount_qty, $discount_price)
+    _validate_discount_scheme_input($discount_qty, $discount_price);
 
     if (!defined $item_code || $item_code eq ''){
         die "Invalid code: You must provide a product code"
@@ -32,25 +32,28 @@ sub _validate_discount_scheme_input {
 
     my ($discount_qty, $discount_price) = @_;
 
-    if ((defined $discount_qty || defined $discount_price) && (!defined $discount_qty || defined $discount_price)){
+    if ((!defined $discount_qty && defined $discount_price) || (defined $discount_qty && !defined $discount_price)) {
         die "Invalid discount scheme: Both discount quantity and price must be set together if a scheme is to be set"
     }
-    if (defined $discount_qty && ($discount_qty !~ /^\d+$/) || $discount_price !~ /^\d+$/){
-        die "Invalid discount quantity: Discount quantities must be non-negative integers";
-    }
-    if (defined $discount_price && ($discount_price !~/^\d+(\.\d+)?$/) || $discount_price <= 0 ){
-        die "Invalid discount price: discount price must be a positive numeric value"
+    if (defined $discount_price){
+
+        if (defined $discount_qty && ($discount_qty !~ /^\d+$/) || $discount_price !~ /^\d+$/){
+            die "Invalid discount quantity: Discount quantities must be non-negative integers";
+        }
+        if (defined $discount_price && ($discount_price !~/^\d+(\.\d+)?$/) || $discount_price <= 0 ){
+            die "Invalid discount price: discount price must be a positive numeric value"
+        }
     }
 }
 
 #getters/setters
 sub get_item_code {
-    my ($self, $code) = @_;
-    $self->{$item_code};
+    my ($self) = @_;
+    return $self->{item_code};
 }
 
 sub set_item_code {
-    my ($self) = @_;
+    my ($self, $item_code) = @_;
     $self->{item_code}= $item_code;
 }
 
@@ -68,17 +71,19 @@ sub get_discount_qty {
     my ($self) = @_;
     return $self->{discount_qty};
 }
-sub set_discount_qty_and_price {
-    my ($self, $discount_qty, $discount_price) = @_;
-    _validate_discount_scheme_input($discount_qty, $discount_price)
-    $self->{discount_qty} = $discount_qty;
-    $self->{discount_price} = $discount_price;
-}
 
 sub get_discount_price {
     my ($self) = @_;
     return $self->{discount_price};
 }
+
+sub set_discount_qty_and_price {
+    my ($self, $discount_qty, $discount_price) = @_;
+    _validate_discount_scheme_input($discount_qty, $discount_price);
+    $self->{discount_qty} = $discount_qty;
+    $self->{discount_price} = $discount_price;
+}
+
 
 
 
