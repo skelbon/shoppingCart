e7d6f8552a3009aa5bb6ea0a6c52f0d86ff9ca60
Joe Gibson <spuriousmeaning@gmail.com>
2024-05-10 13:15:33 +0100

    start if calculate subtotal

--- t/CheckoutTest.pm
+++ t/CheckoutTest.pm
@@ -2,9 +2,13 @@ use strict;
 use warnings;
 use Test::More;
 use Test::Exception;
-use lib '../lib';
-use Checkout;
+use lib "../lib";
 use Product;
+use Checkout;
+use Data::Dumper;
+
+our $basket_items =
+'[{"code":"A","quantity":3},{"code":"B","quantity":3},{"code":"C","quantity":1},{"code":"D","quantity":2}]';
 
 sub test_constructor {
 
@@ -27,7 +31,7 @@ sub test_constructor {
 
     my @products = ();
 
-    #instantiate the product objects from the products_data
+    # Instantiate the product objects from the products_data
     foreach my $product_data (@products_data) {
         my $product = Product->new(
             $product_data->{code},         $product_data->{unit_price},
@@ -38,7 +42,26 @@ sub test_constructor {
 
     my $checkout = Checkout->new( \@products );
     isa_ok( $checkout, 'Checkout', 'Successful object creation' );
+
+    return $checkout;
 }
 
-test_constructor();
+sub test_subtotal_method {
+    my $checkout = test_constructor();
+    my $sub      = $checkout->get_subtotal();
+    return $sub;
+}
+
+sub test_adding_basket_items {
+
+    my $checkout = test_constructor();
+    $checkout->add_basket_items($basket_items);
+    return $checkout;
+}
+
+my $subtotal        = test_subtotal_method();
+my $filled_checkout = test_adding_basket_items();
+
+print Dumper($subtotal);
+
 done_testing();
