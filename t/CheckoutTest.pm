2c527b4b86a8e1de4855a6dcdb04281e67154501
skelbon <spuriousmeaning@gmail.com>
2024-05-12 18:48:22 +0100

    Minor updates

--- t/CheckoutTest.pm
+++ t/CheckoutTest.pm
@@ -5,16 +5,36 @@ use Test::Exception;
 use lib "../lib";
 use Product;
 use Checkout;
-use Data::Dumper;
 
 my $basket_items_json =
 '[{"code":"A","quantity":3},{"code":"B","quantity":3},{"code":"C","quantity":1},{"code":"D","quantity":2}]';
 
-my %test_baskets_json = {
-    1 => { data => '[{"code":"A","quantity":3},{"code":"B","quantity":3},{"code":"C","quantity":1},{"code":"D","quantity":2}]', expected_result => 284}, 
-    2 => { data => '[{"code":"A","quantity":5},{"code":"B","quantity":1},{"code":"C","quantity":0},{"code":"D","quantity":3}]', expected_result => 284}, 
-    3 => { data => '[{"code":"A","quantity":3},{"code":"A","quantity":2},{"code":"A","quantity":1},{"code":"D","quantity":2},{"code":"C","quantity":1}]', expected_result => 284}, 
-};
+my %test_baskets_json = (
+    all_good => {
+        data => '[{"code":"A","quantity":3},{"code":"B","quantity":3},{"code":"C","quantity":1},{"code":"D","quantity":2}]',
+        expected_result => 284
+    },
+    zero_quantity => {
+        data => '[{"code":"A","quantity":5},{"code":"B","quantity":1},{"code":"C","quantity":0},{"code":"D","quantity":3}]',
+        expected_result => 311
+    },
+    multiple_discounted_items => {
+        data => '[{"code":"A","quantity":3},{"code":"B","quantity":4},{"code":"C","quantity":1},{"code":"D","quantity":2}]',
+        expected_result => 309
+    },
+    aggregate_quantities => {
+        data => '[{"code":"A","quantity":3},{"code":"A","quantity":2},{"code":"A","quantity":1},{"code":"D","quantity":2},{"code":"C","quantity":1}]',
+        expected_result => 329
+    },
+    invalid_code => {
+        data => '[{"code":"A","quantity":3},{"code":"E","quantity":2},{"code":"D","quantity":2},{"code":"C","quantity":2}]',
+        expected_result => undef
+    },
+    invalid_quantity => {
+        data => '[{"code":"A","quantity":"invalid"},{"code":"B","quantity":1},{"code":"D","quantity":0},{"code":"C","quantity":1}]',
+        expected_result => 60
+    }
+);
 
 sub test_constructor {
 
@@ -55,19 +75,38 @@ sub test_constructor {
 sub test_subtotal_method {
     my $checkout = test_constructor();
     throws_ok {$checkout->get_subtotal()} qr /no items in the basket/, 'Exception thrown: no items in basket when subtotal called';
-    $checkout->set_basket_items($basket_items_json);
-    is($checkout->get_subtotal(), 284, 'Correct subtotal calculation');
+    
+  
+    $checkout->set_basket_items( $test_baskets_json{all_good}->{data});
+    is($checkout->get_subtotal(), $test_baskets_json{all_good}->{expected_result}, 'Correct subtotal calculation');
+
+    $checkout->set_basket_items( $test_baskets_json{zero_quantity}->{data});
+    is($checkout->get_subtotal(), $test_baskets_json{zero_quantity}->{expected_result}, 'Correct subtotal calculation');
+
+    $checkout->set_basket_items( $test_baskets_json{multiple_discounted_items}->{data});
+    is($checkout->get_subtotal(), $test_baskets_json{multiple_discounted_items}->{expected_result}, 'Correct subtotal calculation');
+
+    $checkout->set_basket_items( $test_baskets_json{aggregate_quantities}->{data});
+    is($checkout->get_subtotal(), $test_baskets_json{aggregate_quantities}->{expected_result}, 'Correct subtotal calculation');
+
+    $checkout->set_basket_items( $test_baskets_json{invalid_quantity}->{data});
+    is($checkout->get_subtotal(), $test_baskets_json{invalid_quantity}->{expected_result}, 'Correct subtotal calculation');
+
+
+}
+
+
 
 
     
-}
+
 
 sub test_setting_basket_items {
 
     my $checkout = test_constructor();
-    lives_ok { $checkout->set_basket_items($basket_items_json) } 'Setting basket items';
-    dies_ok { $checkout->set_basket_items('[{"code":"E","quantity":1}]') } 'Exception thrown: basket with item not found';
-    throws_ok { $checkout->set_basket_items('[{"code":"E","quantity":1}]') } qr/E is not found/, 'Exception thrown: Correct item in exception message';
+    lives_ok { $checkout->set_basket_items($test_baskets_json{all_good}->{data}) } 'Setting basket items';
+    dies_ok { $checkout->set_basket_items($test_baskets_json{invalid_code}->{data}) } 'Exception thrown: basket with item not found';
+    throws_ok { $checkout->set_basket_items($test_baskets_json{invalid_code}->{data})} qr/E is not found/, 'Exception thrown: Correct item in exception message';
     $checkout->set_basket_items($basket_items_json);
     my $basket_items_ref = $checkout->get_basket_items();
     
